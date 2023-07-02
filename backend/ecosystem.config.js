3c458069487f60b746e759d52216c7d5ae147d24
Avok@to34 <vladlen-17@yandex.ru>
2023-07-03 02:45:48 +0300

    add ecosystem

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      key: '~/.ssh/id_ed25519',
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'git@github.com:VladlenFolk/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && npm i && npm run build',
+    },
+  },
+};
