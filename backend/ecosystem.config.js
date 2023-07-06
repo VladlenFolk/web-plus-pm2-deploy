7cbccd3c16ef8e81f8e62f69d122ea5d45c0b1a2
Avok@to34 <vladlen-17@yandex.ru>
2023-07-06 15:10:10 +0300

    deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: REPO_PATH,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'pre-deploy-local': `scp .env .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build`,
     },
   },
