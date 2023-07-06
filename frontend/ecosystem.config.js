e5c6d296e34ccbaaba09616496b147f9e5222f9d
Avok@to34 <vladlen-17@yandex.ru>
2023-07-06 15:08:19 +0300

    deploy

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, REPO_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'app'
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: REPO_PATH,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd source/frontend && npm i && npm run build',
+    },
+  },
+};
